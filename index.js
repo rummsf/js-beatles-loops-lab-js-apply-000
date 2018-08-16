167b0f2a999bc954bd714552fe671dafe1475623
Irum F <irummf@gmail.com>
2018-08-16 16:09:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPlay ([musicians], [instruments]) {
+function theBeatlesPlay (musicians, instruments) {
   for (let i = 0; i < 4; i++)
   console.log(musicians + "plays" + instruments);
 }
