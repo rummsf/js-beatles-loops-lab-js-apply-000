c326fcd72042396578097da0fe0086d3ff5302bb
Irum F <irummf@gmail.com>
2018-08-16 15:40:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay ([musicians], [instruments]) {
+  return(`${musicians} plays ${instruments}`)
+  for (let i = 0; i < 4; i++)
+ console.log(`${musicians} plays ${instruments}`);
+ }
+ function johnLennonFacts ([facts])
+ 
+ function iLoveTheBeatles
