2f19e829e9b49bd16ada745af8b24ea15781c4b9
Irum F <irummf@gmail.com>
2018-08-16 15:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function theBeatlesPlay ([musicians], [instruments]) {
   return(`${musicians} plays ${instruments}`)
-  for (let i = 0; i < 4; i++)
- console.log(`${musicians} plays ${instruments}`);
- }
- 
- for (var i = 1; i < 100; i++) {
-  console.log(musicians plays instruments);
+  for (let i = 0; i < 4; i++) {
+  console.log(musicians + "plays" + instruments);
 }
+
  function johnLennonFacts ([facts])
  
  function iLoveTheBeatles
